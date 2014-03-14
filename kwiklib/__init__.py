a5de6d8dffb52226df9e5a5aa36ea13ff442235b
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-03-14 12:01:36 +0100

    WIP: moved dataio and utils to kwiklib.

---
+++ kwiklib/__init__.py
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+import os
+import sys
+import logging
+
+import utils
+import dataio
+
+from kwiklib.utils import logger as log
+from utils import *
+from dataio import *
---
+++ kwiklib/dataio/__init__.py
@@ -0,0 +1,13 @@
+# FROM KWIK
+from tools import *
+from klustersloader import *
+from kwikloader import *
+
+from utils import *
+from raw import *
+from chunks import *
+from selection import *
+from kwik import *
+from spikecache import *
+from experiment import *
+from tests import *
