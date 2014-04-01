ade7d6d9a4611dff34b5d1b9b7a5e241abbf0868
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-04-01 14:35:17 +0200

    Added comparison of features array in the kwik conversion test.

--- kwiklib/dataio/tests/test_klusterskwik.py
+++ kwiklib/dataio/tests/test_klusterskwik.py
@@ -1,12 +1,16 @@
+import os
 import tables as tb
 from mock_data import *
-from kwiklib.dataio import Experiment
+from kwiklib.dataio import Experiment, read_features
 from kwiklib.dataio.klusterskwik import klusters_to_kwik
 
 def test_conversion_1():
     
     # Convert klusters data to kwik.
     klusters_to_kwik(filename='test', dir=TEST_FOLDER)
+    
+    fet = read_features(os.path.join(TEST_FOLDER, 'test.fet.1'), 
+                        nchannels, fetdim, freq, do_process=False)
     
     with Experiment('test', dir=TEST_FOLDER, mode='r') as exp:
         
@@ -31,4 +35,9 @@ def test_conversion_1():
         assert exp.channel_groups[1].spikes.time_samples[:].sum() > 0
         assert exp.channel_groups[1].spikes.features_masks[:].sum() > 0
         assert exp.channel_groups[1].spikes.waveforms_filtered[:].sum() > 0
+        
+        fet_kwik = exp.channel_groups[1].spikes.features[:]
+        
+        d = (fet / fet_kwik)
+        assert d.max() - d.min() <= 1
         
